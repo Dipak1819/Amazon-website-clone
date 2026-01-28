368994c89b102800143ed3d83690a518baa59b89
Dipak Prasad Kushwaha <dkushwaha1819@gmail.com>
2026-01-28 14:13:01 -0500

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,6 @@
-const products=[
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090 //best practice when calculating money is calculating in cents rather than dollars
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name:'Intermediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        priceCents: 2095
-    },
-    {
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name:'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count:56
-        },
-        priceCents: 799
-    }
-]
-
 //we generated the html below and now its time to save
-
+//the products variable will be coming from the file
+//this is how real world html works we save the data into a file and then generate the file 
 let productsHTML = '';
 
 
