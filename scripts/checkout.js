6295b546cf817496bb9ebbc71de3ff9e18be7e96
Dipak Prasad Kushwaha <dkushwaha1819@gmail.com>
2026-01-31 17:47:57 -0500

    dave data and generate HTML for delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,17 @@
 import {cart,removeFromCart} from '../data/cart.js'
 import {products} from '../data/products.js'
 import {formatCurrency} from './utils/money.js'
-
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
+import {deliveryOptions} from '../data/deliveryOptions.js'
+//this is new syntax, we use it when we only want to use one thing from source like only one miport
+//gives us an object having current dayte and time
+//external libraries generally have documentation we use to see how do we use it
+const today=dayjs()
+  //this will add 7 days to todays date
+const deliveryDate=today.add(7,'days') //okay now wee added 7 days, now accroding to documentation we also have a method called
+//.format which will make it easy to read
+console.log(deliveryDate.format(`dddd, MMMM D`))
+console.log(deliveryDate)
 let cartSummaryHTML=''
 cart.forEach((cartItem)=>{
     const productId=cartItem.productId
@@ -14,12 +24,33 @@ cart.forEach((cartItem)=>{
             matchingProduct=product
         }
     })
-    console.log(matchingProduct)
+    const deliveryOptionId=cartItem.deliveryOptionId
+    deliveryOptionId
+
+    let deliveryOption
+
+    deliveryOptions.forEach((option)=>{
+      if(option.id===deliveryOptionId)
+      {
+        deliveryOption=option;
+      }
+    })
+    
+    const today=dayjs()
+      const deliveryDate=today.add(
+        deliveryOption.deliveryDays,
+        'days'
+      )
+
+      const dateString = deliveryDate.format(
+        'dddd, MMMM D'
+      )
+
     cartSummaryHTML+=`
         <div class="cart-item-container 
         js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
-              Delivery date: Tuesday, June 21
+              Delivery date: ${dateString}
             </div>
 
             <div class="cart-item-details-grid">
@@ -50,44 +81,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
+                ${deliveryOptionsHTML(matchingProduct,cartItem)}
                 </div>
               </div>
             </div>
@@ -95,6 +89,47 @@ cart.forEach((cartItem)=>{
     
     `
 })
+
+function deliveryOptionsHTML(matchingProduct,cartItem){
+  let html= '';
+  deliveryOptions.forEach((deliveryOption)=>{
+    const today=dayjs()
+    const deliveryDate=today.add(
+      deliveryOption.deliveryDays,
+      'days'
+    )
+
+    const dateString = deliveryDate.format(
+      'dddd, MMMM D'
+    )
+
+    const priceString = deliveryOption.priceCents===0
+    ? `Free`
+    : `$${formatCurrency(deliveryOption.priceCents)} -`
+
+    const isChecked = deliveryOption.id===cartItem.deliveryOptionId
+
+    //this is ternary operator if first part is true value is wahterver after question mark else after :
+    html +=
+    `
+    <div class="delivery-option">
+                  <input type="radio"
+                    ${isChecked ? 'checked':''}
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                     ${dateString}
+                    </div>
+                    <div class="delivery-option-price">
+                      ${priceString} Shipping
+                    </div>
+                  </div>
+                </div>
+    `
+  })
+  return html
+}
 
 document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML
 
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,11 @@
 export function formatCurrency(priceCents){
     return (priceCents/100).toFixed(2)
 }
+
+export default formatCurrency
+//now when we import htis we can import this without curly braces
+//ecah file can have only one default export
+//however in your own code its upto you which version of export you want to use
+//the syntax with curly bracses in called named export, some libraris use default export while some library use named export
+
+//not every library has esm version thought, some library need to use script version
